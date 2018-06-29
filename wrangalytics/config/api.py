2156d3ad947b044f80f0ac6e1e282cb9b38bf699
Alex Castrounis <acastrounis@users.noreply.github.com>
2018-06-29 14:31:16 -0500

    Add data file path create function

--- wrangalytics/config/api.py
+++ wrangalytics/config/api.py
@@ -10,3 +10,6 @@ def juptyerConfig(pd, max_columns=500, max_rows = 500, float_format = '{:,.6f}',
     pd.options.display.float_format = float_format.format
     pd.options.display.max_info_rows = 1000
     pd.options.display.max_categories = 500
+
+def createDataFilePath(data_dir='', data_file_name='')
+    return data_dir + data_file_name
