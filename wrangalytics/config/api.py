073c8ece48d801a46216c430b411ae39a0bdcd28
Alex Castrounis <acastrounis@users.noreply.github.com>
2018-06-29 15:03:52 -0500

    Add initial base import function

--- wrangalytics/config/api.py
+++ wrangalytics/config/api.py
@@ -4,6 +4,31 @@ def debug(x):
 ### Notebook Magics
 # %matplotlib inline
 
+def importBasePackages():
+    from IPython.display import display
+
+    import numpy as np
+    import pandas as pd
+
+    import matplotlib.pyplot as plt
+
+    import statsmodels.api as sm
+    import statsmodels.formula.api as smf
+
+    import scipy
+    from scipy import stats
+
+    import sklearn
+
+    import wrangalytics
+    from wrangalytics.config import api as config
+
+    from wrangalytics.converters.api import percent2float as p2f
+    from wrangalytics.converters.api import str2float as s2f
+    from wrangalytics.converters.api import str2int as s2i
+
+    from imp import reload
+
 def juptyerConfig(pd, max_columns=500, max_rows = 500, float_format = '{:,.6f}', max_info_rows = 1000, max_categories = 500):
     pd.options.display.max_columns = 500
     pd.options.display.max_rows = 500
