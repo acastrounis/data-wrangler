7030474f821f79b7e0f33e5e9e01d166a47b54ca
Alex Castrounis <acastrounis@users.noreply.github.com>
2018-06-29 15:12:52 -0500

    Refactor imports

--- wrangalytics/__init__.py
+++ wrangalytics/__init__.py
@@ -6,3 +6,29 @@ name = "wrangalytics"
 # import wrangalytics.converters.api
 # import wrangalytics.eda.viz.api
 # import wrangalytics.eda.stats.api
+
+### Import base functions
+
+from IPython.display import display
+
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+
+import scipy
+from scipy import stats
+
+import sklearn
+
+# import wrangalytics
+# from wrangalytics.config import api as config
+
+from wrangalytics.converters.api import percent2float as p2f
+from wrangalytics.converters.api import str2float as s2f
+from wrangalytics.converters.api import str2int as s2i
+
+from imp import reload
--- wrangalytics/config/api.py
+++ wrangalytics/config/api.py
@@ -1,29 +1,3 @@
-# Import base functions
-
-from IPython.display import display
-
-import numpy as np
-import pandas as pd
-
-import matplotlib.pyplot as plt
-
-import statsmodels.api as sm
-import statsmodels.formula.api as smf
-
-import scipy
-from scipy import stats
-
-import sklearn
-
-# import wrangalytics
-# from wrangalytics.config import api as config
-
-from wrangalytics.converters.api import percent2float as p2f
-from wrangalytics.converters.api import str2float as s2f
-from wrangalytics.converters.api import str2int as s2i
-
-from imp import reload
-
 def debug(x):
     print(x)
 
