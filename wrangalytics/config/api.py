783d19a99ce02b0f6b335637a2f41e846cec8adf
Alex Castrounis <acastrounis@users.noreply.github.com>
2018-06-29 14:22:02 -0500

    Fix magic command in func def

--- wrangalytics/config/api.py
+++ wrangalytics/config/api.py
@@ -1,10 +1,12 @@
 def debug(x):
     print(x)
 
+### Notebook Magics
+# %matplotlib inline
+
 def juptyerConfig(pd, max_columns=500, max_rows = 500, float_format = '{:,.6f}', max_info_rows = 1000, max_categories = 500):
     pd.options.display.max_columns = 500
     pd.options.display.max_rows = 500
     pd.options.display.float_format = float_format.format
     pd.options.display.max_info_rows = 1000
     pd.options.display.max_categories = 500
-    %matplotlib inline
